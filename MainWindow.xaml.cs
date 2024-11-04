b679605ebafc23448b849d73f4338bde27333d4b
kuusuke221 <chemisuke@hotmail.com>
2024-11-04 23:38:36 +0900

    ファイル保存に対応

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Input;
-
+using System.IO;
 
 namespace auto_click_tool
 {
@@ -35,6 +35,27 @@ namespace auto_click_tool
             {
                 Point point = new Point(System.Windows.Forms.Cursor.Position.X, System.Windows.Forms.Cursor.Position.Y);
                 txbCordinates.Text = txbCordinates.Text + point.X.ToString() + ", " + point.Y.ToString() + "\n";
+            }
+        }
+
+        // SaveボタンをクリックするとtxbCordinates.Textの内容をテキストファイルで保存する。
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // ファイルパスを選択するダイアログを表示
+            Microsoft.Win32.SaveFileDialog sfd = new()
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*",
+                DefaultExt = "txt",
+                AddExtension = true
+            };
+
+            bool? result = sfd.ShowDialog();
+
+            // ユーザーがファイルを選択した場合
+            if (result == true)
+            {
+                string path = sfd.FileName;
+                File.WriteAllText(path, txbCordinates.Text);
             }
         }
     }
